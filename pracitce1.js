8c6eeaf5119b8afce320af188b87fa64a57aeee5
yadavamrendra123 <68077237+yadavamrendra123@users.noreply.github.com>
2022-11-07 09:15:05 +0545

    Add files via upload

---
+++ pracitce1.js
@@ -0,0 +1,46 @@
+// let myFirstname="Amrendra"
+// let myLastname="yadav"
+// let fullname=myFirstname+" "+myLastname
+// console.log(fullname)
+
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// function greetingfrnd(){
+//     greet=greeting+", "+name+"!"
+//     console.log(greet)
+// }
+
+// greetingfrnd()
+let counth=0;
+let countg=0
+
+function addh1(){
+    counth=counth+1;
+    document.getElementById("score-h1").textContent=counth;
+}
+
+function addh2(){
+    counth=counth+2;
+    document.getElementById("score-h1").textContent=counth;
+}
+function addh3(){
+    counth=counth+3;
+    document.getElementById("score-h1").textContent=counth;
+}
+
+
+
+function addg1(){
+    countg=countg+1;
+    document.getElementById("score-g1").textContent=countg;
+}
+
+function addg2(){
+    countg=countg+2;
+    document.getElementById("score-g1").textContent=countg;
+}
+function addg3(){
+    countg=countg+3;
+    document.getElementById("score-g1").textContent=countg;
+}
